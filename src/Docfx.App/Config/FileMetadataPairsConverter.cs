8006c364b26a9d281ff5e8493a77b072b543fd9e
deluvas <deluvas1911@gmail.com>
2024-10-30 11:07:07 +0100

    Init

---
+++ src/Docfx.App/Config/FileMetadataPairsConverter.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Docfx.Common;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Docfx;
+
+/// <summary>
+/// JsonConverter for FileMetadataPairs
+/// </summary>
+internal partial class FileMetadataPairsConverter
+{
+}
---
+++ src/Docfx.App/Config/MergeJsonConfigConverter.cs
@@ -0,0 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Docfx;
+
+/// <summary>
+/// JsonConverter for <see cref="MergeJsonConfig"/>.
+/// </summary>
+internal partial class MergeJsonConfigConverter
+{
+}
